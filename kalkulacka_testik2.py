2f9f2762bd2ea28a88dd919de17f17da03eaa7e8
git config --global user.name "Iveta Szaboova <git config --global user.email "ivetka.szaboova@gmail.com>
2024-11-11 20:53:25 +0100

    doplnena kalkulacka pre test

--- kalkulacka_testik2.py
+++ kalkulacka_testik2.py
@@ -1,42 +1,53 @@
 class CustomException(Exception):
     pass
 
+
 def kalkulacka():
-    try:
-        cislo1 = float(input("Zadajte prve cislo: "))
-    except ValueError:
-        print("Písmená sa tam nesmú zadávať, zadajte číslo.")
-        return None
+    while True:
+        prikaz = input("Napíšte 'koniec' pre ukončenie alebo stlačte Enter pre ďalšiu operáciu: ").strip().lower()
+        if prikaz == "koniec":
+            print("Kalkulačka bola ukončená.")
+            break
 
-    operacia = input("Zadajte operaciu (+, -, *, /): ")
-
-    try:
-        cislo2 = float(input("Zadajte druhe cislo: "))
-    except ValueError:
-        print("Písmená sa tam nesmú zadávať, zadajte číslo.")
-        return None
-
-    if operacia == "+":
-        vysledok = cislo1 + cislo2
-        raise CustomException("Jedná sa o vyvolanú výnimku")
-    elif operacia == "-":
-        vysledok = cislo1 - cislo2
-        raise CustomException("Jedná sa o vyvolanú výnimku")
-    elif operacia == "*":
-        raise CustomException("Jedná sa o vyvolanú výnimku")
-    elif operacia == "/":
         try:
-            vysledok = cislo1 / cislo2
-        except ZeroDivisionError:
-            print("Chcel si deliť nulou a to nejde.")
-            return None
-    else:
-        print("Neplatná operácia.")
-        return None
+            cislo1 = float(input("Zadajte prve cislo: "))
+        except ValueError:
+            print("Písmená sa tam nesmú zadávať, zadajte číslo.")
+            continue
 
-    return vysledok
+        operacia = input("Zadajte operaciu (+, -, *, /): ")
 
-try:
-    print(f"Vysledok: {kalkulacka()}")
-except CustomException as e:
-    print(e)
+        try:
+            cislo2 = float(input("Zadajte druhe cislo: "))
+        except ValueError:
+            print("Písmená sa tam nesmú zadávať, zadajte číslo.")
+            continue
+
+        try:
+            if operacia == "+":
+                vysledok = cislo1 + cislo2
+                raise CustomException("Jedná sa o vyvolanú výnimku")
+            elif operacia == "-":
+                vysledok = cislo1 - cislo2
+                raise CustomException("Jedná sa o vyvolanú výnimku")
+            elif operacia == "*":
+                vysledok = cislo1 * cislo2
+                raise CustomException("Jedná sa o vyvolanú výnimku")
+            elif operacia == "/":
+                try:
+                    vysledok = cislo1 / cislo2
+                except ZeroDivisionError:
+                    print("Chcel si deliť nulou a to nejde.")
+                    continue
+            else:
+                print("Neplatná operácia.")
+                continue
+
+            print(f"Vysledok: {vysledok}")
+
+        except CustomException as e:
+            print(e)
+
+
+# Spustenie kalkulačky
+kalkulacka()
